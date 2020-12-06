f9db6f7dad5db6cd560582b888d5b91694c43e64
DashNothing <stokic.toni@gmail.com>
2020-12-06 16:11:21 +0100

    Post form

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,3 +5,7 @@ const URL = "http://localhost:5000/posts";
 export const fetchPosts = () => {
   return axios.get(URL);
 };
+
+export const createPost = (newPost) => {
+  return axios.post(URL, newPost);
+};
