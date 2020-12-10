a835d2792cdd983ce13fd4e60b59616712e0ee0a
DashNothing <stokic.toni@gmail.com>
2020-12-10 16:56:57 +0100

    Update, delete and like post

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -9,3 +9,15 @@ export const fetchPosts = () => {
 export const createPost = (newPost) => {
   return axios.post(URL, newPost);
 };
+
+export const updatePost = (id, updatedPost) => {
+  return axios.patch(`${URL}/${id}`, updatedPost);
+};
+
+export const deletePost = (id) => {
+  return axios.delete(`${URL}/${id}`);
+};
+
+export const likePost = (id) => {
+  return axios.patch(`${URL}/${id}/likePost`);
+};
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Post from "./post/Post";
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 
 import useStyles from "./styles";
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => {
     return state.posts;
   });
@@ -12,11 +13,21 @@ const Posts = () => {
 
   console.log(posts);
 
-  return (
-    <>
-      <h1>Posts</h1>
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      align-items="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid item key={post._id} xs={12} sm={6}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
