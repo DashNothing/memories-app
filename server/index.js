6978bd72cdb5d61813082a7214c8e7f6d18843a7
DashNothing <stokic.toni@gmail.com>
2020-12-10 23:43:15 +0100

    make it better

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -19,8 +22,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/posts", postRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello memories api");
+});
+
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
